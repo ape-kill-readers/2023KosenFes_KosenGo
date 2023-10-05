3ab6552108425014acdaa2b41ba6c82f306d31d6
yuha-yuha <ishizushimadev@gmail.com>
2023-10-05 09:20:09 +0900

    もろもろ変更

---
+++ src/views/StartView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type router from '@/router';
+
+
+</script>
+
+<template>
+    <main>
+    <div class="btn"><button @click="() => {$router.push('#')}">Start</button></div>
+    </main>
+</template>
+
+<style lang="scss">
+.btn {
+    margin: auto;
+    button{
+        width: 300px;
+        height: 300px;
+    }
+}
+
+</style>
