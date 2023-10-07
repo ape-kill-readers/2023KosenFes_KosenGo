87bd4ef168104915718c21f501b61ba012c0fc4f
yuha-yuha <ishizushimadev@gmail.com>
2023-10-07 12:32:45 +0900

    quize footer の作成

---
+++ src/components/QuizeFooter.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+
+    const UserAnswer = ref('')
+</script>
+
+<template>
+    <input v-model="UserAnswer" class="user_answer_input">
+</template>
+
+<style>
+    .user_answer_input {   
+        width: 30%;
+        height: 30%;
+        margin: 16px;
+    }
+    .footer {
+        display: flex;
+        justify-content: center;
+    }
+</style>
