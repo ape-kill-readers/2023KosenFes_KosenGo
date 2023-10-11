bd621cc9db4af07d2475dc62f25264ba8f1c48d0
Meri11Ra1 <R.t.00293794@gmail.com>
2023-10-12 01:10:56 +0900

    mainContentのtemplateとcssを実装した。

--- src/views/QuizeView.vue
+++ src/views/QuizeView.vue
@@ -7,9 +7,78 @@ const QuizeProgressCount = useProgressCounterStore()
 const route = useRoute();
 
 
+
 </script>
 
 <template>
+    <label class="currentStatus">
+        <div>
+            <div class="modeBox">
+                <h2 class="modeText">10</h2>
+            </div>
+            <div class="leftBox">
+                <h2 class="leftText">残3</h2>
+            </div>
+        </div>
+    </label>
 
+    <div class="question">
+        <div class="questionField">
+            <div class="questionBox">
+                
+            </div>
+        </div>
+    </div>
+    
     <button @click="QuizeProgressCount.Increment()">ss</button>
 </template>
+
+<style lang="scss">
+    .currentStatus {
+        display: flex;
+        flex-direction: row-reverse;
+    }
+    .modeBox {
+        display: flex;
+        background-color: #666666;
+        align-items: center;
+        justify-content: center;
+        width: 10vh;
+        height: 10vh;
+    }
+    .modeText {
+        color: white;
+    }
+    .leftBox {
+        display: flex;
+        background-color: #D9D9D9;
+        align-items: center;
+        justify-content: center;
+        width: 10vh;
+        height: 10vh;
+    }
+    .leftText {
+        color: black;
+    }
+    .question {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-top: 13vh;
+    }
+    .questionField {
+        display: flex;
+        position: absolute;
+    }
+    .questionBox {
+        background-color: #d9d9d9;
+        padding-right: 35vh;
+        padding-left: 35vh;
+        padding-top: 20vh;
+        padding-bottom: 20vh;
+        /*
+        width: 775px;
+        height: 345px;
+        */
+    }
+</style>
