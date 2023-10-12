b8225cecf7473cf1912d89e03279fa6c62ff200d
Meri11Ra1 <R.t.00293794@gmail.com>
2023-10-13 00:52:04 +0900

    update: スタート画面の説明要素を整えた

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -5,10 +5,12 @@ import type router from '@/router';
 </script>
 
 <template>
-    <div class="explain_box">
-        <p class="explain_content">
-
-        </p>
+    <div class="explain_view">
+        <div class="explain_box">
+            <text class="explain_text">
+                俺ら仲いいなああああああああああああああああああああああああああああああああああああああ
+            </text>
+        </div>
     </div>
 </template>
 
@@ -16,13 +18,28 @@ import type router from '@/router';
     .main {
         height: 70%;
     }
+    .explain_view {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
     .explain_box {
-        width: 94%;
-        height: 74%;
-        
-        text-align: center;
-        margin: 0 auto;
-        margin-top: auto;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 120vh;
+        height: 60vh;
+        margin-top: 5vh;
         background-color: white;
+        word-wrap: break-word;
+        overflow: hidden;
+    }
+    .explain_text {
+        width: 120vh;
+        padding-right: 4vh;
+        padding-left: 4vh;
+        text-align: center;
+        font-size: 4vh;
+        color: red;
     }
 </style>
