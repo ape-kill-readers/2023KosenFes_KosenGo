b87bf8e8d7c7ea0302a2b4caaefb4f19e053f8e8
yuha-yuha <ishizushimadev@gmail.com>
2023-10-12 21:39:35 +0900

    updaet:headerの要素を追加

--- src/components/StartHeader.vue
+++ src/components/StartHeader.vue
@@ -1,3 +1,20 @@
 <template>
-    aaa
+    <p class="header_content"> スタート画面 </p>
 </template>
+
+<style lang="scss">
+.header {
+    height: 10%;
+    display: flex;
+    justify-content: center;
+    .header_content{
+        margin: auto 0;
+        color: white;
+        text-shadow: 0px 4px 4px black;
+        font-family: Abel;
+        font-size: 70px; 
+        font-weight: 400;
+        line-height: normal; 
+    }
+}
+</style>
