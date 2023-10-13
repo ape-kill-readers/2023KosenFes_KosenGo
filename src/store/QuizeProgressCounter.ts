6e8665201bad120f878ddb126a16af3017c53dd7
Meri11Ra1 <R.t.00293794@gmail.com>
2023-10-13 12:26:01 +0900

    fix: ブラウザUndo時にルーティングが変わってない問題

--- src/store/QuizeProgressCounter.ts
+++ src/store/QuizeProgressCounter.ts
@@ -1,10 +1,14 @@
 import { defineStore } from "pinia";
 import { ref, type Ref } from "vue";
 
+const quizeLen = 3 //後で設定ファイルみたいなの作る
+
 export const useProgressCounterStore = defineStore('ProgressCounter', () => {
     const ProgressCount = ref(0) as Ref<number>
     function Increment() {
-      ProgressCount.value++
+      if (ProgressCount.value < quizeLen) {
+        ProgressCount.value++
+      }
     }
 
     return {ProgressCount, Increment}
