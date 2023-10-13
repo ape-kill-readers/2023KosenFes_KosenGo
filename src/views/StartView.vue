8b3705fbb2efc9a3ebf23ad5eaa10e335957638d
yuha-yuha <ishizushimadev@gmail.com>
2023-10-13 11:45:13 +0900

    サーバからfetchできるようにした

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import type router from '@/router';
+import {useQuizeDataStore} from '../store/QuizeData'
+const QuizeData = useQuizeDataStore()
+
 
 
 </script>
 
 <template>
-    <button class="btn" @click="() => {$router.push('quize')}">Start</button>
+    <button class="btn" @click="() => {
+        QuizeData.QuizeFetch()
+        console.log(QuizeData.QuizeData)
+        $router.push('quize')}">Start</button>
 </template>
 
 <style lang="scss" scoped>
