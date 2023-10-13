eb2bb384590db5b340c605ab20d298cde8d44081
Meri11Ra1 <R.t.00293794@gmail.com>
2023-10-13 11:46:00 +0900

    update: 文字入力ができる状態でエンターキーを押してね

--- src/components/StartFooter.vue
+++ src/components/StartFooter.vue
@@ -9,17 +9,29 @@
 </script>
 
 <template>
+    <div class="press_enter_view">
+        <text class="press_enter_text">文字入力ができる状態でEnterキーを押してね</text>
+    </div>
     <input v-model="UserAnswer" @keyup.enter="() => {$router.push('quize')}" class="user_answer_input">
 </template>
 
 <style>
-    .user_answer_input {   
-        width: 30%;
-        height: 30%;
-        margin: 16px;
+    .user_answer_input {  
+        all: unset; 
+        width: 50%;
+        height: 50%;
+        margin-left: 25%;
+        text-align: center;
+        background-color: #d9d9d9;
+        font-size: 4vh;
+        color: black
+    }
+    .press_enter_text {
+        color: black;
+        font-size: 2.5vh;
     }
     .footer {
-        display: flex;
-        justify-content: center;
+        display:block;
+        background-color: white;
     }
 </style>
