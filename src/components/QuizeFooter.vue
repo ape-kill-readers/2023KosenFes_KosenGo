fffa9191ab76a6cd06a4401447569cd1c0f91e1f
yuha-yuha <ishizushimadev@gmail.com>
2023-10-14 13:12:13 +0900

    update:正解判定をできるようにした

--- src/components/QuizeFooter.vue
+++ src/components/QuizeFooter.vue
@@ -1,18 +1,31 @@
 <script setup lang="ts">
+    import {useQuizeDataStore} from '../store/QuizeData'
+    import { useProgressCounterStore } from '@/store/QuizeProgressCounter';
     import { ref } from 'vue';
+    const QuizeData = useQuizeDataStore()
+    const ProgressCounter = useProgressCounterStore() 
 
     const UserAnswer = ref('')
     const consoled = () => {
         console.log("println")
         UserAnswer.value = ''
     }
+
+    function JudgeAnswer() {
+    if (QuizeData.QuizeData.ans == UserAnswer.value){
+        QuizeData.QuizeFetch()
+        ProgressCounter.Increment()
+        UserAnswer.value = ''
+    }
+}
 </script>
 
 <template>
     <div class="press_enter_view">
         <text class="press_enter_text"></text>
     </div>
-    <input v-model="UserAnswer" @keyup.enter="consoled()" class="user_answer_input">
+    <input v-model="UserAnswer" @keyup.enter="JudgeAnswer()" class="user_answer_input">
+    <p id="judge_result"></p>
 </template>
 
 <style>
