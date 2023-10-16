b063e159bd12758eb06ade822fdad5da62c2535e
chusehi <yu.nakamura.gamegod@gmail.com>
2023-10-16 14:27:38 +0900

    FinishViewを修正

--- src/views/FinishView.vue
+++ src/views/FinishView.vue
@@ -9,44 +9,11 @@ const route = useRoute();
 </script>
 
 <template>
-    <div class="statusDepartment">
-        <div>
-            <div class="ansTimeBox">
-                <h2 class="ansTimeText">10</h2>
-            </div>
-            <div class="leftBox">
-                <h2 class="leftText">残3</h2>
-            </div>
-        </div>
-    </div>
+    <dev>
 
+    </dev>
 </template>
 
 <style lang="scss">
-    .statusDepartment {
-        display: flex;
-        flex-direction: row-reverse;
-    }
-    .ansTimeBox {
-        display: flex;
-        background-color: #666666;
-        align-items: center;
-        justify-content: center;
-        width: 10vh;
-        height: 10vh;
-    }
-    .ansTimeText {
-        color: white;
-    }
-    .leftBox {
-        display: flex;
-        background-color: #D9D9D9;
-        align-items: center;
-        justify-content: center;
-        width: 10vh;
-        height: 10vh;
-    }
-    .leftText {
-        color: black;
-    }
+    
 </style>
