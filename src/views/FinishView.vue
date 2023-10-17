60f65ca575d54f52bb66e4afe039acde49f79501
chusehi <yu.nakamura.gamegod@gmail.com>
2023-10-17 17:22:50 +0900

    ALL CLEARを実装し終わりました

--- src/views/FinishView.vue
+++ src/views/FinishView.vue
@@ -33,209 +33,129 @@ const route = useRoute();
 
 <style lang="scss">
 .div {
-  padding-right: 20px;
-  padding-left: 20px;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
 }
+
 .div-2 {
   gap: 20px;
   display: flex;
-}
-@media (max-width: 991px) {
-  .div-2 {
+  justify-content: center;
+  align-items: center;
+  @media (max-width: 991px) {
     flex-direction: column;
-    align-items: stretch;
-    gap: 0px;
   }
 }
+
 .column {
+  flex-grow: 1;
   display: flex;
   flex-direction: column;
-  line-height: normal;
-  width: 34%;
-  margin-left: 0px;
-}
-@media (max-width: 991px) {
-  .column {
+  justify-content: center;
+  align-items: center;
+  @media (max-width: 991px) {
     width: 100%;
   }
 }
+
 .div-3 {
   display: flex;
-  margin-top: 2px;
-  flex-grow: 1;
-  align-items: flex-start;
-  gap: 3%;
+  gap: 10px;
 }
-@media (max-width: 991px) {
-  .div-3 {
-    margin-top: 50px;
-    justify-content: center;
-  }
-}
-.div-4 {
-  justify-content: center;
-  color: rgba(254, 173, 15, 0.76);
-  text-align: center;
-  font-family: Inter, sans-serif;
-  font-size: 200px;
-  font-weight: 700;
-  align-self: stretch;
-  width: 128px;
-}
-@media (max-width: 991px) {
-  .div-4 {
-    font-size: 40px;
-  }
-}
-.div-5 {
-  justify-content: center;
-  color: rgba(243, 235, 36, 0.87);
-  text-align: center;
-  font-family: Inter, sans-serif;
-  font-size: 200px;
-  font-weight: 700;
-  align-self: stretch;
-  width: 128px;
-}
-@media (max-width: 991px) {
-  .div-5 {
-    font-size: 40px;
-  }
-}
+
+.div-4,
+.div-5,
 .div-6 {
   justify-content: center;
-  color: #caf324;
   text-align: center;
-  font-family: Inter, sans-serif;
-  font-size: 200px;
+  font-family: 'Inter', sans-serif;
+  font-size: 10rem;
   font-weight: 700;
-  align-self: stretch;
-  width: 128px;
-}
-@media (max-width: 991px) {
-  .div-6 {
-    font-size: 40px;
+  margin-top: -20vh;
+  -webkit-text-stroke: 3px #000;
+  @media (max-width: 991px) {
+    font-size: 2rem;
+    margin-top: -20vh;
+    margin-bottom: 20vh;
   }
 }
-.column-2 {
-  display: flex;
-  flex-direction: column;
-  line-height: normal;
-  width: 66%;
-  margin-left: 20px;
+
+.div-4 {
+    color: #FEAD0F;
 }
-@media (max-width: 991px) {
-  .column-2 {
+
+.div-5 {
+    color: #F3EB24;
+}
+
+.div-6 {
+    color: #CAF324;
+}
+
+.column-2 {
+  flex-grow: 1;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  margin-left: 20px;
+  @media (max-width: 991px) {
+    margin-left: 0;
     width: 100%;
   }
 }
+
 .div-7 {
   display: flex;
-  flex-grow: 1;
-  align-items: flex-start;
-  gap: 9px;
+  gap: 10px;
 }
-@media (max-width: 991px) {
-  .div-7 {
-    max-width: 100%;
-    margin-top: 50px;
-    flex-wrap: wrap;
-    justify-content: center;
-  }
-}
-.div-8 {
-  justify-content: center;
-  color: #40d38d;
-  text-align: center;
-  font-family: Inter, sans-serif;
-  font-size: 200px;
-  font-weight: 700;
-  align-self: stretch;
-  width: 128px;
-}
-@media (max-width: 991px) {
-  .div-8 {
-    font-size: 40px;
-  }
-}
-.div-9 {
-  justify-content: center;
-  color: #72fbda;
-  text-align: center;
-  font-family: Inter, sans-serif;
-  font-size: 200px;
-  font-weight: 700;
-  align-self: stretch;
-  margin-top: 2px;
-  width: 128px;
-}
-@media (max-width: 991px) {
-  .div-9 {
-    font-size: 40px;
-  }
-}
-.div-10 {
-  justify-content: center;
-  color: #52b6ee;
-  text-align: center;
-  font-family: Inter, sans-serif;
-  font-size: 200px;
-  font-weight: 700;
-  align-self: stretch;
-  margin-top: 2px;
-  width: 128px;
-}
-@media (max-width: 991px) {
-  .div-10 {
-    font-size: 40px;
-  }
-}
-.div-11 {
-  justify-content: center;
-  color: #5071e8;
-  text-align: center;
-  font-family: Inter, sans-serif;
-  font-size: 200px;
-  font-weight: 700;
-  align-self: stretch;
-  margin-top: 2px;
-  width: 128px;
-}
-@media (max-width: 991px) {
-  .div-11 {
-    font-size: 40px;
-  }
-}
-.div-12 {
-  justify-content: center;
-  color: #ac45c6;
-  text-align: center;
-  font-family: Inter, sans-serif;
-  font-size: 200px;
-  font-weight: 700;
-  align-self: stretch;
-  margin-top: 2px;
-  width: 128px;
-}
-@media (max-width: 991px) {
-  .div-12 {
-    font-size: 40px;
-  }
-}
+
+.div-8,
+.div-9,
+.div-10,
+.div-11,
+.div-12,
 .div-13 {
   justify-content: center;
-  color: #e481fd;
   text-align: center;
-  font-family: Inter, sans-serif;
-  font-size: 200px;
+  font-family: 'Inter', sans-serif;
+  font-size: 10rem;
   font-weight: 700;
-  align-self: stretch;
-  margin-top: 2px;
-  width: 128px;
-}
-@media (max-width: 991px) {
-  .div-13 {
-    font-size: 40px;
+  margin-top: -20vh;
+  -webkit-text-stroke: 3px black;
+  @media (max-width: 991px) {
+    font-size: 2rem;
+    margin-top: -20vh;
+    margin-bottom: 20vh;
   }
 }
+
+.div-8 {
+    color: #40D38D;
+}
+
+.div-9 {
+    color: #72FBDA;
+}
+
+.div-10 {
+    color: #52B6EE;
+}
+
+.div-11 {
+    color: #5071E8;
+}
+
+.div-12 {
+    color: #AC45C6;
+}
+
+.div-13 {
+    color: #E481FD;
+}
+
 </style>
